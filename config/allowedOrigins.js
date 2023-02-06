0e75783d18fa7ecab61a43bdad2a72f8b5132e09
WATBO Yvan <104699044+Zamorano237@users.noreply.github.com>
2023-02-06 11:46:19 +0100

    allow vercel origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'https://qhseacademieandconsulting.onrender.com',
   'https://qhseacademieandconsulting.vercel.app/',
+  'https://qhseacademieandconsulting.vercel.app'
 ];
 
 module.exports = allowedOrigins;
