01ac9e0bebc3ecd7388b14b3c1e6425b4ba74c20
WATBO Yvan <104699044+Zamorano237@users.noreply.github.com>
2023-02-13 00:51:23 +0100

    allow origin firebase

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,10 +2,10 @@
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://qhseacademieandconsulting.onrender.com',
   'http://www.qhseacademieandconsulting.com',
   'https://www.qhseacademieandconsulting.com',
   'https://qhseacademieandconsulting.vercel.app',
+  'https://qhseacademieandconsulting.com/',
 ];
 
 module.exports = allowedOrigins;
